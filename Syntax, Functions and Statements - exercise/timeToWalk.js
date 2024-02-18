165dc22c9cb9a3517807b4ecd6de38a46ceb096a
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-18 18:08:00 +0200

    delete

--- Syntax, Functions and Statements - exercise/timeToWalk.js
+++
@@ -1,5 +0,0 @@
-function timeToWalk (steps, footprint, speed) {
-
-}
-timeToWalk(4000, 0.60, 5);
-// timeToWalk(2564, 0.70, 5.5);
--- Syntax, Functions and Statements - exercise/vadilityCheker.js
+++
@@ -1,6 +0,0 @@
-function vadilityCheker(x1, y1, x2, y2) {
-
-    
-}
-vadilityCheker(3, 0, 0, 4);
-// vadilityCheker(2, 1, 1, 1);
