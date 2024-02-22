42459baf476f722271b1b43d5eb44215b8dc676a
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-22 23:35:38 +0200

    commit solution

--- DOM introduction.exercise/02. Pascal or Camel Case/solution.js
+++ DOM introduction.exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-  //TODO...
+
+  let inputText = document.getElementById('text').value;
+  let convention = document.getElementById('naming-convention').value;
+  let resultRef = document.getElementById('result');
+  inputText = inputText.toLowerCase();
+  let textArray = inputText.split(' ');
+  let result = '';
+
+  if (convention == 'Camel Case') {
+
+    result = textArray.shift();
+    textArray.forEach(word => {
+      result += word[0].toUpperCase() + word.substring(1).toLowerCase();
+    })
+
+  } else if (convention == 'Pascal Case') {
+
+    textArray.forEach(word => {
+      result += word[0].toUpperCase() + word.substring(1);
+    })
+
+  } else {
+    result = 'Error!';
+  }
+  
+  resultRef.textContent = result;
 }
+
+
