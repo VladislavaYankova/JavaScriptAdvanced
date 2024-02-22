91ed3252b52fec0ac794cd2339a297105d683639
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-22 17:59:43 +0200

    commit solutions

--- DOM introduction.lab/04.Show More/ShowMore.js
+++ DOM introduction.lab/04.Show More/ShowMore.js
@@ -1,3 +1,6 @@
 function showText() {
-    // TODO
+
+    document.getElementById('text').style.display = '';
+    document.getElementById('more').style.display = 'none';
+
 }
--- DOM introduction.lab/05.Colorize Table/ColorizeTable.js
+++ DOM introduction.lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,9 @@
 function colorize() {
-    // TODO
+    
+    let rows = Array.from(document.getElementsByTagName('tr'));
+
+    for (let i = 1; i < rows.length; i+=2) {
+        let row = rows[i];
+        row.style.backgroundColor = 'teal';
+    }
 }
