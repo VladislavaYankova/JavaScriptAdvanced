aa50989271bed992e6dd913776e52f2cc7c824bf
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-22 18:34:12 +0200

    Update SumTable.js

--- DOM introduction.lab/06.Sum Table/SumTable.js
+++ DOM introduction.lab/06.Sum Table/SumTable.js
@@ -1,3 +1,14 @@
 function sumTable() {
 
+    let total = 0;
+
+    let rows = Array.from(document.getElementsByTagName('tr'));
+
+    for (let i = 1; i < rows.length - 1; i++) {
+        let cols = rows[i].children;
+        let cost = cols[cols.length - 1].textContent;
+        total += Number(cost);
+    }
+
+    document.getElementById('sum').textContent = total;
 }
