e67773dc08026c6bf75c5fc35a325f0f46568f2a
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-22 17:44:34 +0200

    commit solutions

--- DOM introduction.lab/02.Collect List Items/CollectListItems.js
+++ DOM introduction.lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,16 @@
 function extractText() {
-    // TODO
+
+    let result = [];
+    let list = document.getElementById('items');
+
+    let items = Array.from(list.children);
+
+    for (let item of items) {
+        result.push(item.textContent);
+    }
+
+    let textArea = document.getElementById('result');
+    textArea.value = result.join('\n');
+
+
 }
