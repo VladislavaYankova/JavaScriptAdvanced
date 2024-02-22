9d1789aab835ee76e4c3dc5c2c84d4b1ca1bd474
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-22 17:27:27 +0200

    solve 01. Edit element

--- DOM introduction.lab/01.Edit Element/EditElement.js
+++ DOM introduction.lab/01.Edit Element/EditElement.js
@@ -1,3 +1,6 @@
-function editElement() {
-    // TODO
+function editElement(element, match, replacer) {
+
+    let content = element.textContent;
+    element.textContent = content.split(match).join(replacer);
+
 }
