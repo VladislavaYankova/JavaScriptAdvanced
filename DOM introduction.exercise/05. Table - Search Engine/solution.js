558edbdb2e98208076919cd78ec6d45fccaf6b52
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-23 16:56:16 +0200

    commit solution

--- DOM introduction.exercise/05. Table - Search Engine/solution.js
+++ DOM introduction.exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,22 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   let typeArea = document.getElementById('searchField');
+   let tableRows = Array.from(document.querySelectorAll('tbody tr'));
 
+   function onClick() {
+      let searchText = typeArea.value;
+      for (let tableRow of tableRows) {
+         let tableData = Array.from(tableRow.querySelectorAll('td'));
+         for (let data of tableData) {
+            if (data.textContent.includes(searchText)) {
+               tableRow.classList.add('select');
+               break;
+            } else {
+               tableRow.classList.remove('select');
+            }
+         }
+      }
+      typeArea.value = '';
    }
 }
