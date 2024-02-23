18986866c445d21e7373059e50f1ee6cbec5cf47
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-23 11:37:17 +0200

    commit solution

--- DOM introduction.exercise/03. Accordion/accordion.js
+++ DOM introduction.exercise/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    
+    let button = document.getElementsByClassName('button')[0];
+    let text = document.getElementById('extra');
+
+    if (text.style.display == 'none') {
+        text.style.display = 'block';
+        button.textContent = 'Less';
+    } else {
+        text.style.display = 'none';
+        button.textContent = 'More';
+    }
 }
