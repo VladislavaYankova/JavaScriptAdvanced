5ecda45130abaa9da5c37435a999703bdb7d8faa
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-25 23:18:47 +0200

    Update app.js

--- DOM Manipulations and events.lab/02. Delete-From-Table/app.js
+++ DOM Manipulations and events.lab/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let result = document.getElementById('result');
+    let input = document.querySelector('[name="email"]');
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+
+    for (let row of rows) {
+        let email = row.children[1].textContent;
+
+        if (email == input.value) {
+            row.remove();
+            result.textContent = 'Deleted.';
+        } else {
+            result.textContent = 'Not found.';
+        }
+    }
+
+    input.value = '';
 }
