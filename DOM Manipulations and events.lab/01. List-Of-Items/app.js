376544375bdceb0cfa35e3c6e0b150994ff3e22f
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-25 22:25:16 +0200

    commit solution

--- DOM Manipulations and events.lab/01. List-Of-Items/app.js
+++ DOM Manipulations and events.lab/01. List-Of-Items/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    let input = document.getElementById('newItemText');
+    let list = document.getElementById('items');
+    let li = document.createElement('li');
+
+    li.textContent = input.value;
+    list.appendChild(li);
+
+    input.value = '';
 }
