799ff1be4e274df5518b6bc54cdbcef282b3dbb9
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-26 14:26:23 +0200

    Update app.js

--- DOM Manipulations and events.lab/03. Add-Delete/app.js
+++ DOM Manipulations and events.lab/03. Add-Delete/app.js
@@ -1,3 +1,25 @@
 function addItem() {
-    //TODO...
+    let input = document.getElementById('newItemText');
+    let list = document.getElementById('items');
+    let li = document.createElement('li');
+
+    if (input.value.length == 0) {
+        return;
+    }
+
+    li.textContent = input.value;
+    list.appendChild(li);
+
+    let deleteBtn = document.createElement('a');
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.href = '#';
+    li.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener('click', onclick);
+
+    function onclick(event) {
+        li.remove();
+    }
+
+    input.value = '';
 }
