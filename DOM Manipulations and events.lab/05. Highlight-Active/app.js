e280f66478069840b4d2d8415159bc156b39bdb2
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-26 15:07:04 +0200

    Update app.js

--- DOM Manipulations and events.lab/05. Highlight-Active/app.js
+++ DOM Manipulations and events.lab/05. Highlight-Active/app.js
@@ -1,3 +1,20 @@
 function focused() {
-    console.log('TODO:...');
+
+    let typeArea = Array.from(document.querySelectorAll('[type="text"]'));
+
+    for (let input of typeArea) {
+        input.addEventListener('focus', () => {
+            let parent = input.parentElement;
+            parent.classList.add('focused');
+
+        });
+
+        input.addEventListener('blur', () => {
+            let parent = input.parentElement;
+            parent.classList.remove('focused');
+
+        })
+    }
+
+
 }
