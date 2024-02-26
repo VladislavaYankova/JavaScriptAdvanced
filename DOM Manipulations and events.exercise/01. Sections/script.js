175788d19145c80ac4935be97557a0e661d9bbf9
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-26 22:03:17 +0200

    Update script.js

--- DOM Manipulations and events.exercise/01. Sections/script.js
+++ DOM Manipulations and events.exercise/01. Sections/script.js
@@ -1,3 +1,23 @@
 function create(words) {
-   console.log('TODO:...');
+
+   let content = document.getElementById('content');
+
+   for (let word of words) {
+      let divEl = document.createElement('div');
+      content.appendChild(divEl);
+      let pEl = document.createElement('p');
+      pEl.textContent = word;
+      divEl.appendChild(pEl);
+      pEl.style.display = 'none';
+
+      divEl.addEventListener('click', () => {
+         if (pEl.style.display == 'none') {
+            pEl.style.display = 'block';
+         } else {
+            pEl.style.display = 'none';
+         }
+      })
+
+      content.appendChild(divEl);
+   }
 }
