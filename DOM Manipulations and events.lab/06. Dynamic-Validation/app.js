aab308e14b768e4996cf58642a23c2aca857b4e4
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-26 15:26:45 +0200

    Update app.js

--- DOM Manipulations and events.lab/06. Dynamic-Validation/app.js
+++ DOM Manipulations and events.lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    console.log('TODO:...');
+   let input = document.getElementById('email');
+   let regex = /[a-z]+@[a-z]+.[a-z]+/g;
+
+   input.addEventListener('change', () => {
+    if (!input.value.match(regex)) {
+        input.classList.add('error');
+    } else {
+        input.classList.remove('error');
+    }
+   });
 }
