07b870a0a69de0e63e8daa99ac5363a20fe3ec6a
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2024-02-26 22:58:23 +0200

    Update solution.js

--- DOM Manipulations and events.exercise/02. Time-Converter/solution.js
+++ DOM Manipulations and events.exercise/02. Time-Converter/solution.js
@@ -1,4 +1,29 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let buttons = Array.from(document.querySelectorAll('[type="button"]'));
+
+    for (let button of buttons) {
+       button.addEventListener('click', convert);
+    }
+
+    function convert(event) {
+        let input = event.currentTarget.parentElement.children[1];
+        let inputValue = Number(input.value);
+        let elementId = input.id;
+
+        switch(elementId) {
+            case 'days' : result(inputValue); break;
+            case 'hours': result(inputValue / 24); break;
+            case 'minutes': result(inputValue / 24 / 60); break;
+            case 'seconds': result(inputValue / 24 / 60 / 60); break;
+        }
+    }
+
+    function result(days) {
+        let inputs = document.querySelectorAll('[type = "text"]');
+        inputs[0].value = days;
+        inputs[1].value = days * 24;
+        inputs[2].value = days * 24 * 60;
+        inputs[3].value = days * 24 * 60 * 60;
+    }
 }
